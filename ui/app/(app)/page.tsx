42d18208c625987d5de230a8b9d8e73221a34bd4
Shubhang Sharma <rustedshader@gmail.com>
2025-01-04 04:27:16 +0530

    Home Page Improvements

--- ui/app/(app)/page.tsx
+++ ui/app/(app)/page.tsx
@@ -1,6 +1,10 @@
 "use client"
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
+import DotPattern from '@/components/ui/dot-pattern';
+import GridPattern from '@/components/ui/grid-pattern';
+import { WarpBackground } from '@/components/ui/warp-background';
 import AboutUs from '@/content/about.mdx'
+import { cn } from '@/lib/utils';
 import { MDXProvider } from '@mdx-js/react'
 import Image from 'next/image'
 import { useEffect, useRef } from 'react';
@@ -17,7 +21,12 @@ export default function Home() {
 
   return (
     <>
-      <div className='bg-gradient-to-b from-40% from-black via-green-900 to-black'>
+      <div className=''>
+        <DotPattern
+          className={cn(
+            "[mask-image:radial-gradient(450px_circle_at_center,white,transparent)]",
+          )}
+        />
         <section className="relative flex min-h-[70vh] items-center justify-center">
           <div className="container relative z-10 flex max-w-[64rem] flex-row items-center justify-between gap-8">
             <div className="flex-1">
@@ -54,6 +63,9 @@ export default function Home() {
           className="container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
         >
           <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+            <WarpBackground>
+              <div>Testing Warp</div>
+            </WarpBackground>
             <h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
               Past Events
             </h2>
