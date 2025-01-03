958e767120dfd74438b835d3bb82bb3eff838008
Shubhang Sharma <rustedshader@gmail.com>
2025-01-04 01:27:51 +0530

    Header Completed , Added Mdx Support and Refactored

--- ui/app/(app)/page.tsx
+++ ui/app/(app)/page.tsx
@@ -1,5 +1,41 @@
+"use client"
+import AboutUs from '@/content/about.mdx'
+import { MDXProvider } from '@mdx-js/react'
+
+
 export default function Home() {
   return (
-    <div>This is HomePage</div>
+    <>
+      <section className="flex min-h-screen items-center justify-center">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Welcome to Microsoft Technical Community
+          </h1>
+        </div>
+      </section>
+      <section
+        id="features"
+        className="container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+            About Us
+          </h2>
+          <MDXProvider>
+            <AboutUs />
+          </MDXProvider>
+        </div>
+      </section>
+      <section
+        id="features"
+        className="container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+            Past Events
+          </h2>
+        </div>
+      </section>
+    </>
   )
 }
