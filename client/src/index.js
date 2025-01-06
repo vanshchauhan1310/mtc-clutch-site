64b22c5f422f4e5973149f2af4a2a09dcd7b6600
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2025-01-07 01:32:46 +0530

    Deleted Unnecessary file

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
